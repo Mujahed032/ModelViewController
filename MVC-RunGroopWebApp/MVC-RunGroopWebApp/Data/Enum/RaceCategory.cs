841250dce5df49c3832bd3ba015a5b490e90bed3
Mujahed032 <mohdmujahed032@gmail.com>
2023-08-16 15:40:06 +0530

    Added Models and Seed data
    Added Models and Seed data

---
+++ MVC-RunGroopWebApp/MVC-RunGroopWebApp/Data/Enum/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_RunGroopWebApp.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        Fivek,
+        Tenk,
+        HalfMarathon
+    }
+}
--- MVC-RunGroopWebApp/MVC-RunGroopWebApp/Program.cs
+++ MVC-RunGroopWebApp/MVC-RunGroopWebApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MVC_RunGroopWebApp.Data;
+
 namespace MVC_RunGroopWebApp
 {
     public class Program
@@ -8,8 +11,17 @@ namespace MVC_RunGroopWebApp
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
+            if (args.Length == 1 && args[0].ToLower() == "seeddata")
+            {
+                //await Seed.SeedUsersAndRolesAsync(app);
+                Seed.SeedData(app);
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
